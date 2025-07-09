b3da78f60430b2cd72da647acfe724a03d5365d5
AdrianGerman <adrian.german1019@gmail.com>
2025-07-09 08:27:11 -0600

    app settings for react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,13 @@
-import { useState } from "react"
-import GroupList from "./components/GroupList"
-import EntryList from "./components/EntryList"
 import Header from "./components/Header"
+import { RoutesApp } from "./routes/RoutesApp"
 
 export default function App() {
-  const [activeGroup, setActiveGroup] = useState(null)
-
   return (
     <div className="bg-gray-950 min-h-screen text-white">
       <Header />
       <main className="px-4 py-6">
-        <div className="max-w-5xl mx-auto">
-          {activeGroup ? (
-            <EntryList
-              group={activeGroup}
-              goBack={() => setActiveGroup(null)}
-            />
-          ) : (
-            <GroupList selectGroup={setActiveGroup} />
-          )}
+        <div className="max-w-3xl mx-auto">
+          <RoutesApp />
         </div>
       </main>
     </div>
