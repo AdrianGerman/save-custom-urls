165f23d1f4d7ec5006a1fd0ec2fa78e76ccb3586
AdrianGerman <adrian.german1019@gmail.com>
2025-07-09 08:26:48 -0600

    add react router in main and routesApp

---
+++ src/routes/RoutesApp.jsx
@@ -0,0 +1,23 @@
+import { Routes, Route, useNavigate, useParams } from "react-router-dom"
+import GroupList from "../components/GroupList"
+import EntryList from "../components/EntryList"
+
+export function RoutesApp() {
+  return (
+    <Routes>
+      <Route path="/" element={<GroupListWithNav />} />
+      <Route path="/grupo/:group" element={<EntryListWithNav />} />
+    </Routes>
+  )
+}
+
+function GroupListWithNav() {
+  const navigate = useNavigate()
+  return <GroupList selectGroup={(group) => navigate(`/grupo/${group}`)} />
+}
+
+function EntryListWithNav() {
+  const { group } = useParams()
+  const navigate = useNavigate()
+  return <EntryList group={group} goBack={() => navigate(-1)} />
+}
