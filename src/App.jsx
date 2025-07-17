1cde15b5d0bba89b7f6239fe8f7f9e855a93a413
AdrianGerman <adrian.german1019@gmail.com>
2025-07-17 08:18:44 -0600

    add background page and edit header

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import { RoutesApp } from "./routes/RoutesApp"
 
 export default function App() {
   return (
-    <div className="bg-gray-950 min-h-screen text-white">
+    <div className="relative min-h-screen text-white overflow-hidden">
+      <div className="absolute top-0 left-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]" />
+
       <Header />
-      <main className="px-4 py-6">
+      <main className="px-4 py-6 relative z-10">
         <div className="max-w-3xl mx-auto">
           <RoutesApp />
         </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 export default function Header() {
   return (
-    <header className="w-full bg-gray-900 text-white py-6 px-6 shadow-md fade-in">
+    <header className="w-full bg-gray-950/60 text-white py-4 px-6 shadow-md fade-in">
       <div className="max-w-6xl mx-auto flex items-center justify-between">
         <Link
           to="/"
@@ -13,9 +13,6 @@ export default function Header() {
         </Link>
 
         <div className="flex items-center gap-4 text-sm">
-          <Link to="/sobre-mi" className="hover:text-gray-300 transition">
-            Sobre mí
-          </Link>
           <Link
             to="/json"
             className="hover:text-gray-300 transition px-3 py-1 rounded bg-gray-700 hover:bg-gray-600"
